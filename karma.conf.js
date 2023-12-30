243430abfb376e1da4a366ef7123d82db9fd71f7
Devin <devinchandula@gmail.com>
2023-12-30 16:08:53 +0530

    Rebased

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vira-mean'),
+      dir: require('path').join(__dirname, './coverage/ng-master-template'),
       subdir: '.',
       reporters: [
         { type: 'html' },
